8dba6844d3d76ca2a00da15a2c9a2c1541975b89
AlexChay <AlexChay@gmail.com>
2015-06-14 08:45:27 +0300

    Add config parameter "fields"

--- lib/logstash/filters/oui.rb
+++ lib/logstash/filters/oui.rb
@@ -2,7 +2,7 @@
 require "logstash/filters/base"
 require "logstash/namespace"
 
-# This example filter will replace the contents of the default 
+# This example filter will replace the contents of the default
 # message field with whatever you specify in the configuration.
 #
 # It is only intended to be used as an example.
@@ -19,16 +19,21 @@ class LogStash::Filters::Oui < LogStash::Filters::Base
   #
   config_name "oui"
   milestone 1
-  
+
   # The source field to parse
   config :source, :validate => :string, :default => "message"
+
+  # An array of oui fields to be included in the event.
+  # The following are available:
+  # id, organization, address1, address2, address3 and country.
+  config :fields, :validate => :array
 
   # The target field to place all the data
   config :target, :validate => :string, :default => "oui"
 
   public
   def register
-    # Add instance variables 
+    # Add instance variables
     require 'oui'
   end # def register
 
@@ -39,9 +44,11 @@ class LogStash::Filters::Oui < LogStash::Filters::Base
     if ! oui.nil?
       event[@target] = Hash.new
       oui.each do |key, value|
-        event[@target][key.to_s] = value
+        next if value.nil? || (value.is_a?(String) && value.empty?)
+        if @fields.nil? || @fields.empty? || @fields.include?(key.to_s)
+          event[@target][key.to_s] = value
+        end
       end
-
       # filter_matched should go in the last line of our successful code
       filter_matched(event)
     end
